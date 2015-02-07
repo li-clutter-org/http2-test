dd3dab3faad3316d6cb934ef096b0811d37d287f
alcidesv <alcidesv@zunzun.se>
2015-02-07 09:52:39 +0100

    Parsing HAR files complete

--- hs-src/Rede/HarFiles/DataStructure.hs
+++ hs-src/Rede/HarFiles/DataStructure.hs
@@ -15,7 +15,6 @@ module Rede.HarFiles.DataStructure(
     ,queryString 
     ,reqUrl 
     ,onContentLoad 
-    ,comment
     ,onLoad 
     ,vpName 
     ,vpVersion
@@ -25,12 +24,16 @@ module Rede.HarFiles.DataStructure(
 import           Control.Applicative
 import           Control.Monad
 import qualified Control.Lens        as L
+import           Control.Lens        ( (^.), to )
 import           Control.Lens.TH     (makeLenses)
+import           Text.Printf         (printf)
 
 import           Data.Aeson
-import           Data.ByteString     (ByteString)
-import           Data.ByteString.Char8 (pack)
-
+import           Data.Aeson.Types       (Parser)
+import           Data.ByteString        (ByteString)
+import qualified Data.ByteString.Lazy   as LB
+import           Data.ByteString.Char8  (pack)
+import           Data.Text(Text)
 
 
 -- Which kind of string?
@@ -54,22 +57,28 @@ data Har_Header = Har_Header {
 
 data Har_Response = Har_Response {
     _status       :: !Int 
-    ,_content     :: !HereString
-    ,_respHeaders :: !Har_Header
+    ,_content     :: !Har_Content
+    ,_respHeaders :: ![Har_Header]
+    }
+
+
+-- There are more fields hidden in this...
+data Har_Content   = Har_Content {
+    _contentText  :: ! (Maybe HereString)
     }
 
 
 data Har_Request = Har_Request {
     _method       :: !HereString
-    ,_reqHeaders  :: !Har_Header
-    ,_queryString :: !HereString
+    ,_reqHeaders  :: ![Har_Header]
+    ,_queryString :: ![Har_QueryString]
     ,_reqUrl      :: !HereString
     ,_reqBody     :: !HereString
     }
 
 
 data Har_Outer = Har_Outer {
-    _log:: !Har_Log
+    _harLog:: !Har_Log
     }
 
 
@@ -81,17 +90,23 @@ data Har_Page = Har_Page {
    }
 
 
+data Har_QueryString = Har_QueryString {
+   _qsName               ::  !HereString 
+   ,_qsValue             ::  !HereString
+   }
+
+
 data Har_PageTimings = Har_PageTimings {
     -- Milliseconds
     _onContentLoad     :: !Int 
-    ,_comment          :: !HereString 
+    --,_comment          :: !HereString 
     -- Milliseconds
     ,_onLoad           :: !Int 
    }
 
 
 data Har_VersionPair = Har_VersionPair {
-   _vpVersion          :: !Int 
+   _vpVersion          :: !HereString 
    ,_vpName            :: !HereString
    }
 
@@ -112,22 +127,27 @@ makeLenses ''Har_VersionPair
 makeLenses ''Har_Log
 makeLenses ''Har_Request
 makeLenses ''Har_PageTimings
+makeLenses ''Har_QueryString
+makeLenses ''Har_Content
+
+errorParse  :: Value -> Parser a
+errorParse  x = error $ printf "NotGoodHere: %s" $ show x
 
 
 instance FromJSON Har_VersionPair where 
 
     parseJSON (Object v)  = Har_VersionPair <$>
-        v   .: "version"               <*>
-        ( pack <$>  v .: "name" )
+        ( pack <$>  v .: "version" )    <*>
+        ( pack <$>  v .: "name"    )
 
-    parseJSON _   =  mzero 
+    parseJSON  x =  errorParse x
 
 
 instance FromJSON Har_PageTimings where 
 
     parseJSON (Object v)  = Har_PageTimings <$> 
          v .: "onContentLoad"          <*>
-         (pack <$> v .: "comment")     <*>
+         -- (pack <$> v .: "comment")     <*>
          v .: "onLoad"
 
 
@@ -135,14 +155,16 @@ instance FromJSON Har_Header where
 
     parseJSON (Object v) = Har_Header <$> 
         (pack <$> v .: "name"  )       <*>
-        (pack <$> v .: "value" ) 
+        (pack <$> v .: "value" )        
+
+    parseJSON  x         = errorParse x
 
 
 instance FromJSON Har_Response where 
 
     parseJSON (Object v) = Har_Response <$>
         v .: "status"                  <*>
-        (pack <$> v .: "content")      <*>
+        v .: "content"                 <*>
         v .: "headers"
 
 
@@ -151,7 +173,7 @@ instance FromJSON Har_Request where
     parseJSON (Object v) = Har_Request <$> 
         (pack <$> v .: "method" )      <*>
         v .: "headers"                 <*>
-        (pack <$> v .: "queryString" ) <*>
+        v .: "queryString"             <*>
         (pack <$> v .: "url" )         <*>
         (pure "")
 
@@ -169,9 +191,9 @@ instance FromJSON Har_Log where
 instance FromJSON Har_Page where 
 
     parseJSON (Object v) = Har_Page  <$>
-        (pack <$> v .: "_startedDateTime") <*>
+        (pack <$> v .: "startedDateTime") <*>
         v .: "pageTimings" <*>
-        (pack <$> v .: "pageId") <*>
+        (pack <$> v .: "id") <*>
         (pack <$> v .: "title")
 
 
@@ -182,7 +204,32 @@ instance FromJSON  Har_Entry where
         v .: "response"
 
 
+instance FromJSON Har_QueryString where 
+
+    parseJSON (Object v) = Har_QueryString <$>
+        (pack <$> v .: "name" )     <*>
+        (pack <$> v .: "value")
+
+
+instance FromJSON Har_Content where 
+    parseJSON (Object v) = Har_Content <$>
+        ( (liftA pack) <$> v .:? "text" )
+
+
 instance FromJSON Har_Outer where 
 
     parseJSON (Object v) = Har_Outer <$> 
         v .: "log"
+
+
+------ Some small functions to try this out 
+test_01 :: IO ()
+test_01 = do 
+    file_contents <- LB.readFile "tests/bitbucket.org.har"
+    case (eitherDecode file_contents :: Either String Har_Outer ) of 
+    
+        Right doc_model -> do 
+            putStrLn $ printf "File has %d entries" $ doc_model ^. harLog . entries . to length
+
+        Left  msg       -> do 
+            putStrLn $ printf "Apparently parsing failed: %s" msg
