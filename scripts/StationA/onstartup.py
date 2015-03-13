4e1dee3a98c7f6830504bb884c9278b90ec74b5f
alcidesv <alcidesv@zunzun.se>
2015-03-13 13:14:40 +0100

    Changing location

--- scripts/StationA/onstartup.py
+++ scripts/StationA/onstartup.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python
+
+from __future__ import print_function
+
 import shlex
 import subprocess
 import re
+import sys
 import time
 
 #google-chrome
@@ -15,17 +19,20 @@ def run(cmdstr):
     p = subprocess.Popen(pieces)
     return p
 
-chrome_process = run("google-chrome")
+chrome_process = run("google-chrome --disable-gpu")
 time.sleep(5.0)
 s = tool("xwininfo -tree -root")
 mo = re.search(r"\s+(0x[a-f0-9]+) \".*?Google Chrome\"", s)
-winid = mo.group(1)
-print("Win id:", winid)
-tool("xdotool windowsize --sync {0} 100% 100%".format(winid))
-tool("xdotool click --window {0} 1".format(winid))
-time.sleep(0.5)
-tool("xdotool key --window {0} \"ctrl+shift+i\"".format(winid))
-time.sleep(0.5)
-# Get chrome as full-screen, so to make taking screenshots easier.
-tool("xdotool key --window {0} \"F11\"".format(winid))
-chrome_process.wait()
+if mo is None:
+	print("Couldn't find Chrome windows")
+else:
+	winid = mo.group(1)
+	print("Win id:", winid)
+	tool("xdotool windowsize --sync {0} 100% 100%".format(winid))
+	tool("xdotool click --window {0} 1".format(winid))
+	time.sleep(0.5)
+	tool("xdotool key --window {0} \"ctrl+shift+i\"".format(winid))
+	time.sleep(0.5)
+	# Get chrome as full-screen, so to make taking screenshots easier.
+	tool("xdotool key --window {0} \"F11\"".format(winid))
+	chrome_process.wait()
--- scripts/StationB/onstartup.py
+++ scripts/StationB/onstartup.py
@@ -16,18 +16,21 @@ def run(cmdstr):
     p = subprocess.Popen(pieces)
     return p
 
-chrome_process = run("google-chrome")
+chrome_process = run("google-chrome --disable-gpu")
 # Let's start also a daemon process to update the dns masq
 run("python /home/{user}/StationB/configure_dnsmasq.py".format(user=os.environ["USER"]))
 time.sleep(5.0)
 s = tool("xwininfo -tree -root")
 mo = re.search(r"\s+(0x[a-f0-9]+) \".*?Google Chrome\"", s)
-winid = mo.group(1)
-print("Win id:", winid)
-tool("xdotool click --window {0} 1".format(winid))
-time.sleep(0.5)
-tool("xdotool key --window {0} \"ctrl+shift+i\"".format(winid))
-time.sleep(0.5)
-# Get chrome as full-screen, so to make taking screenshots easier.
-tool("xdotool key --window {0} \"F11\"".format(winid))
-chrome_process.wait()
+if mo is None:
+	print("Couldn't find Chrome windows")
+else:
+	winid = mo.group(1)
+	print("Win id:", winid)
+	tool("xdotool click --window {0} 1".format(winid))
+	time.sleep(0.5)
+	tool("xdotool key --window {0} \"ctrl+shift+i\"".format(winid))
+	time.sleep(0.5)
+	# Get chrome as full-screen, so to make taking screenshots easier.
+	tool("xdotool key --window {0} \"F11\"".format(winid))
+	chrome_process.wait()
